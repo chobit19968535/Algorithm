c601f805d8eb94ff4176093709dc65c1a0032925
Lyciris <41323791+chobit19968535@users.noreply.github.com>
2024-12-27 16:53:30 +0800

    Update flood_fill.py

--- 003_Flood-Fill/flood_fill.py
+++ 003_Flood-Fill/flood_fill.py
@@ -16,16 +16,15 @@ for i in range (0, height):
     row = np.random.choice((0,1), size = stride, p =[.75, .25])
     img[i,:] = row
 
-start_x = np.random.randint(0, stride)
-start_y = np.random.randint(0, height)
+start_x = np.random.randint(0, stride-1)
+start_y = np.random.randint(0, height-1)
 
 X = (start_y, start_x)
 
 while(img[X] != 0):
-    start_x = np.random.randint(0, stride)
-    start_y = np.random.randint(0, height)
+    start_x = np.random.randint(0, stride-1)
+    start_y = np.random.randint(0, height-1)
     X = (start_y, start_x)
-visted = set()
 
 def Flood_fill(img, visted, start):
     queue = deque([start])
@@ -34,7 +33,6 @@ def Flood_fill(img, visted, start):
         if img[node] == 0:    
             print(node)
             img[node] = 2
-            visted.add(node)
             candicates = []
             candicates.append((node[0]-1, node[1]))
             candicates.append((node[0], node[1]-1))
@@ -48,4 +46,4 @@ def Flood_fill(img, visted, start):
                         queue.append(candicate)
             
     
-Flood_fill(img, visted, X)
+Flood_fill(img, X)
