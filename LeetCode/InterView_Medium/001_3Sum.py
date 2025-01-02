601e4cde086ca15054e54a9063c82a98f2f3a634
Lyciris <41323791+chobit19968535@users.noreply.github.com>
2025-01-02 17:10:59 +0800

    Update 001_3Sum.py

--- LeetCode/InterView_Medium/001_3Sum.py
+++ LeetCode/InterView_Medium/001_3Sum.py
@@ -5,16 +5,16 @@ Created on Thu Jan  2 14:11:23 2025
 @author: Lycoris
 """
 
-dc = dict()
 nums = [-1,0,1,2,-1,-4]
-l = len(nums)
-for i in nums:
-    if i not in dc:
-        dc[i] = 1
-    else:
-        dc[i] = dc[i]+1
-        
 
-for z in dc:
-    for i in range(0, l):
-        
+res = []
+nums.sort()
+for i in range(len(nums)):
+    if i > 0 and nums[i] == nums[i-1]:
+        continue
+    seen = set()
+    for j in range(i+1, len(nums)):
+        complement = -nums[i] - nums[j]
+        if complement in seen:
+            res.append([nums[i], nums[j], complement])
+        seen.add(nums[j])
